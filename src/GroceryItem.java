8012037c533495fad1e4203ac75057bcf1c471b8
CaseyRounseville <mariarounseville@cox.net>
2014-11-24 08:18:07 -0700

    Update GroceryItem.java

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -1,1 +1,27 @@
 // class to represent a grocery item
+public class GroceryItem
+{
+  int quantity;
+  String name;
+  double price;
+  public GroceryItem(String n,int q,double p)
+  {
+    quantity=q;
+    name=n;
+    price=p;
+  }
+  public double getCost()
+  {
+    double cost=quantity*price;
+    return cost;
+  }
+  public void setQuantity(int q)
+  {
+    quantity=q;
+  }
+  public String toString()
+  {
+    String list=quantity+" "+name+" @ $"+price+"=$"+getCost();
+    return list;
+  }
+}
